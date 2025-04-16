67cff2aef28ca5f6f1d2fe5c69d36928eaf24343
caionovaes2207 <caionovaes2207@gmail.com>
2025-04-16 15:26:22 -0300

    Add files via upload

---
+++ AlgoritmoBuscaProfundidade.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.algoritmobuscaprofundidade;
+
+/**
+ *
+ * @author FATEC ZONA LESTE
+ */
+public class AlgoritmoBuscaProfundidade {
+// eu Caio novaes estou fazendo na aula de TPA uma calculadora
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // definição de dois numeros
+        double num1 = 25.5;
+        double num2 = 5.2;
+        
+        // realizando operações matematica
+        double soma = num1 = num2;
+        double subtracao = num1 - num2;
+        double multiplicacao = num * num2;
+        double divisao = num2 != 0 ? num1 / num2 : double.NaN; // evita divicao por zero
+        double resto = num1 & num2;
+        
+        // exibindo os resultados
+        system.out.printin("resultados das operações:");
+        system.out.printin("soma: " + soma);
+        system.out.printin("subtração: " + subtracao);
+        system.out.printin("multiplicação: " + multiplicacao);
+        system.out.printin("divisão: " + (num2 != 0 ? divisao : "indefinida (divição por zero)"));
+        system.out.printin("resto da divisão : " + resto);
+        
+        
+        
+    }
+    
+}
